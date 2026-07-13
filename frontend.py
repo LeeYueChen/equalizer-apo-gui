88eaf6fb4b15dc8cb4a06c484a6922459e34542d
LeeYueChen <LEE.YUE.CHEN@student.mmu.edu.my>
2026-07-13 18:35:11 +0800

    completed writing to file functions

--- frontend.py
+++ frontend.py
@@ -7,9 +7,13 @@ def format_preamp(preamp):
     return string
 
 def generate_config(gains):
+    count = 1
+    string = ""
     frequency = [20, 25, 32, 40, 50, 63, 80, 100, 125, 160, 200, 250, 315, 400, 500, 630, 800, 1000, 1250, 1600, 2000, 2500, 3150, 4000, 5000, 6300, 8000, 10000, 12500, 16000, 20000]
-    for i in range(31):
-        format_filter(i+1, frequency[i], gains)
+    for freq, gain in zip(frequency, gains):
+        string += format_filter(count, freq, gain)+"\n"
+        count += 1
+    return string
 
 with open("preset.txt", "w", encoding="utf-8") as file:
     file.write("Filter  " , number ,": ON  PK       Fc     " , frequency ,"Hz   Gain  " , gain, " dB  Q 4.32")
